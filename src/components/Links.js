706e75dafbb7b780819a781d7d30834b5ec946f7
Jonatandb <jonatandb@gmail.com>
2020-07-01 20:10:12 -0300

    Corrijo importación de archivo de conexión a Firebase Agrego funcionalidad que muestra links creados

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LinkForm from "./LinkForm";
 import { db } from "../firebase";
 
 export default function Links() {
+  const [links, setLinks] = useState([]);
+
+  useEffect(() => {
+    getLinks();
+  }, []);
+
+  const getLinks = () => {
+    const docs = [];
+    db.collection("links").onSnapshot((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        docs.push({ ...doc.data(), id: doc.id });
+      });
+      setLinks(docs);
+    });
+  };
+
   const addOrEditLink = async (linkObject) => {
     await db.collection("links").doc().set(linkObject);
   };
 
   return (
     <div>
-      <LinkForm addOrEditLink={addOrEditLink} />
-      <h1>Links</h1>
+      <div className="col-md-4 p-2">
+        <LinkForm addOrEditLink={addOrEditLink} />
+      </div>
+      <div className="col-md-8 p-2">
+        {links.map((link) => (
+          <div className="card mb-1" key={link.id}>
+            <div className="card-body">
+              <h4>{link.name}</h4>
+              <p>{link.description}</p>
+              <a href={link.url} target="_blank" rel="noopener noreferrer">
+                Go to Website
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import firebase from "./firebase";
+import "./firebase";
 import "bootswatch/dist/cyborg/bootstrap.min.css";
 
 ReactDOM.render(
